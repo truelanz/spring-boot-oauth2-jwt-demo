67bb9548d3f56dda67831712ce629bceb4e14c48
truelanz <devalanoliveira@gmail.com>
2025-01-13 12:37:47 -0300

    Spring Security checklist. service and projection - not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,5 +1,7 @@
 package com.devsuperior.demo.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +10,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +29,8 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
     public String getAuthority() {
         return authority;
     }
